d2e79ac4d9b96c90869af5b6c54c150445088499
alexanderevgrafov <alexander.evgrafov@gmail.com>
2019-05-14 23:35:48 +0300

    Project to life

---
+++ src/Bootstrap.jsx
@@ -0,0 +1,39 @@
+import React from 'react-type-r'
+import {
+    Form as BsForm,
+    Card as BsCard,
+    Button as BsButton
+} from 'react-bootstrap';
+import cx from 'classnames'
+
+const Form = BsForm,
+      Card = BsCard;
+
+Form.ControlLinked = ( { valueLink, onChange, accepts, value, ...props } ) =>
+    <BsForm.Control
+        onChange={e => {
+            if( !accepts || e.target.value.match( accepts ) ) {
+                onChange && onChange( e );
+                valueLink && valueLink.set( e.target.value );
+            }
+        }}
+        value={valueLink ? valueLink.value : value}
+        {...props}
+    />;
+
+Card.SquareImg = ( { src, className, ...props } ) =>
+    <BsCard.Img
+        as='div'
+        className={cx( 'card_square_img', className )}
+        style={{ backgroundImage : 'url(' + src + ')' }}
+        {...props}
+    />;
+
+const MyButton = ( { label, children, ...props } ) =>
+    <BsButton {...props}>{label || children}
+    </BsButton>;
+
+export { MyButton as Button }
+
+export { Form, Card }
+export * from 'react-bootstrap'
--- src/app.js
+++ src/app.js
@@ -1,20 +1,66 @@
-import 'type-r/globals'
-import React, { define } from 'react-mvx'
+import React from 'react-type-r'
 import * as ReactDOM from 'react-dom'
+import { define } from 'type-r'
+//import { Select, Slider } from 'ui/Controls'
+import {
+    Container, Row, Col,
+    Badge,
+    Card,
+    Modal, Form, Button
+} from './Bootstrap'
+//import cx from 'classnames'
+import { _t } from 'app/translate'
+import 'scss/app.scss'
+
+@define
+class ConfigModel extends Record {
+    static attributes = {
+        tl    : 2,
+        th    : 5,
+        ton   : 10,
+        toff  : 10,
+        read  : 180,
+        log   : 1800,
+        flush : 7200
+    }
+}
 
 @define
 class Application extends React.Component {
     static state = {
-        counter : 0
+        conf : ConfigModel
     };
 
     render() {
-        return (
-            <div>
-                {this.state.counter}
-                <button onClick={() => this.state.counter++}>Increase</button>
-            </div>
-        );
+        const { conf } = this.state;
+
+        return <Container>
+            <Row>
+                <Col>
+                    <Row label='T low'>
+                        <Form.ControlLinked valueLink={conf.linkAt( 'tl' )}/>
+                    </Row>
+                    <Row label='T high'>
+                        <Form.ControlLinked valueLink={conf.linkAt( 'th' )}/>
+                    </Row>
+                    <Row label='ON min'>
+                        <Form.ControlLinked valueLink={conf.linkAt( 'ton' )}/>
+                    </Row>
+                    <Row label='OFF min'>
+                        <Form.ControlLinked valueLink={conf.linkAt( 'toff' )}/>
+                    </Row>
+                    <Row label='Read each'>
+                        <Form.ControlLinked valueLink={conf.linkAt( 'read' )}/>
+                    </Row>
+                    <Row label='Log each'>
+                        <Form.ControlLinked valueLink={conf.linkAt( 'log' )}/>
+                    </Row>
+                    <Row label='Flush log each'>
+                        <Form.ControlLinked valueLink={conf.linkAt( 'flush' )}/>
+                    </Row>
+                </Col>
+            </Row>
+        </Container>;
     }
 }
 
